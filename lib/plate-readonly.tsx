fc565c86dbdb7dc81c1eac7c0611f6226990d610
Vojtěch Jungmann <vojta.jungmann@gmail.com>
2024-07-25 13:23:54 +0200

    fix: imports

--- lib/plate-readonly.tsx
+++ lib/plate-readonly.tsx
@@ -5,11 +5,6 @@ import './styles/globals.css'
 import { Editor } from './components/plate-ui/editor'
 import { Plate } from '@udecode/plate-common'
 
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-
-import { TooltipProvider } from './components/plate-ui/tooltip'
-
 import { plugins } from './plugins'
 import type { PlateEditorProps } from './plate'
 
